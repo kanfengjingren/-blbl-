24388dc18729f44108b496c5e43156adb706d6fe
kanfengjingren <3441018715@qq.com>
2025-12-07 23:53:56 +0800

    搜索框请求发送完成

--- BLBL实现.md
+++ BLBL实现.md
@@ -7,3 +7,93 @@
 ### 引入网站图标
 使用这样一行类型是icon的链接，然后图片地址写成对应的地址
 `<link rel="icon" href="./img/blbl.ico">`
+### 用express去建立一个本地的服务器
+使用`npm i express`安装好express框架
+构建出express的基本结构 
+```
+//1、引入express
+const express = require('express');
+//创建应用对象
+const app = express();
+//3、创建路由规则
+//request是对请求报文的封装
+//response是对响应报文的封装
+app.get('/', (request, response) => {
+
+    response.send('hello Ajax');
+});
+//4、监听端口启动服务
+app.listen(8000, () => {
+    console.log("服务器已经启动,8000端口监听中...");
+
+})
+```
+### 使用nodemon启动服务器，实时改变服务器的状态和变化
+`npm install -g nodemon`安装
+`nodemon server.js`启动服务器端
+启用之后，服务器会自动刷新更改
+
+### 使用tailwindcss4
+构建流程：
+![tailwindcss安装](/笔记img/tailwindcss.png "tailwindcss安装")
+* 注意html中引入的是output.css
+
+### git提交和同步的准备
+使用vs自带的git工具，创建库之后，提交。注意的是我在github上已经创建了库，所以治理使用远程去选取库而不是创建一个新的库
+![添加远程存储库](/笔记img/添加远程存储库.png "添加远程存储库")
+
+## html+css
+总体的html分为三部分：头部、分类区、主体区
+![blbl](/笔记img/blbl.png)
+### 1.头部也分为左中右三个部分，左边和右边的功能类似，中间部分则是搜索框。同时整个头部在滚动到一定范围之后需要作为fixed定位。左右两边分区需要使用定位来实现一些下拉框效果。中间的搜索框，我的想法是简单的把输入的数据发送到服务端，让服务端经过一些处理之后，再发送回来
+服务端：
+```
+app.use(express.text());
+//创建一个处理数据的路由规则
+app.post('/search', (req, res) => {
+    console.log('收到搜索请求');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //用req来读取一个接受过来的信息
+    console.log(req.body);
+    //发送的纯字符串请求
+
+
+
+    const textData = JSON.stringify(chooseArr(req.body, textArr));
+
+
+    //返回什么数据？10条有关信息，把req.body的每个字符拆开来，去textArr里面寻找
+    res.send(textData);
+})
+
+function chooseArr(str, arr) {
+    const dataSet = new Set();
+    let charArr = str.split('');
+
+    for (let index = 0; index < charArr.length; index++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j].includes(charArr[index])) {
+                dataSet.add(arr[j]);
+            }
+
+        }
+
+    }
+    const dataArr = Array.from(dataSet)
+
+
+
+    return dataArr;
+}
+```
+### 2.分类区
+![分类区](/笔记img/分类区.png)
+第一次使用grid布局，感觉还不错，不过这里是写在原生css里卖弄的，后面我再改到tailwindcss里面
+
+### 3.主内容
+![主内容](/笔记img/主内容.png)
+使用grid布局，然后轮播图是跨行跨列
+下方的主要内容，我的想法是，页面滚动到一定位置再发送ajax请求JSON数据转换成对象再实现一个加载，这个好像就是懒加载？
+
+# 注意
+1、在JavaScript中直接操作style属性时，属性名需要使用驼峰命名法，而不是CSS中的短横线命名法。
--- server.js
+++ server.js
@@ -5,21 +5,130 @@ const app = express();
 
 //将视频封装成一个对象数组，然后把对象JSON化，最后传JSON
 
+const vedioList = [
+    {
+        url: `./img/多罗西新书.jpg`,
+        head: '【新书首发】下班后签2000本书是什么体验？|《烦恼退散》',
+        upName: 'up多罗西123'
+    },
+    {
+        url: `./img/TheWall.jpg`,
+        head: '《迷墙》-平克弗洛伊德 全专整柜附字幕',
+        upName: 'NikateKiwi'
+    },
+    {
+        url: `./img/American football.jpg`,
+        head: '【HiRes无损音乐】 American Football（美国足球）',
+        upName: '看风景人'
+    },
+    {
+        url: `./img/月之暗面.jpg`,
+        head: '5.1 杜比全景声 Pink Floyd - The Dark Side of the Moon',
+        upName: '看风景人'
+    },
+    {
+        url: `./img/black in back.jpg`,
+        head: 'AC/DC交流/直流《Back In Black》专辑',
+        upName: '看风景人'
+    }
 
-const videoTest = {
-    url : `./img/多罗西新书.jpg`,
-    head:'【新书首发】下班后签2000本书是什么体验？|《烦恼退散》',
-    upName:'up多罗西123'
-};
-const data = JSON.stringify(videoTest);
+
+
+];
+
 //3、创建路由规则
 app.get('/server', (request, response) => {
+    //随机选取五个对象组成新的对象数组，然后JSON化返回
+    let vedioTest = [];
+    for (let i = 0; i < 5; i++) {
+        let randomInRange = Math.floor(Math.random() * (vedioList.length));
+        console.log(randomInRange);
+        
+        vedioTest.push(vedioList[randomInRange]);
+    }
 
+    //每个对象都应该有个状态？被选到了就要改成另一种，然后刷新的时候发送一个请求，服务端再全部重置状态
+    
+    const data = JSON.stringify(vedioTest);
     console.log('收到请求');
     //设置响应头   设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.send(data);
 });
+
+const textArr = ['a',
+    'A',
+    'ab',
+    'abc',
+    'abcd',
+    'abcde',
+    'abcdef',
+    'abcdefg',
+    'abcdefgh',
+    'abcdefghi',
+    'abcdefghij',
+    '2026年省考难度',
+    '梅西迈阿密超燃夺冠记录',
+    '牧神记平叛篇正式开启',
+    '萨拉赫炮轰利物浦',
+    '揭开西夏陵神秘面纱',
+    '情久 vs TTG 挑战者杯',
+    '辽宁舰正常训练遭日本飞机滋扰',
+    '美国战略布局有何转变',
+    '动物城薪资和体型有何关联',
+    '小航母076究竟有多强',
+    'pink Floyd',
+    '小木曾雪菜',
+    '五更琉璃',
+    'American football',
+
+];
+
+app.use(express.text());
+//创建一个处理数据的路由规则
+app.post('/search', (req, res) => {
+    console.log('收到搜索请求');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //用req来读取一个接受过来的信息
+    console.log(req.body);
+    //发送的纯字符串请求
+
+
+
+    const textData = JSON.stringify(chooseArr(req.body, textArr));
+
+
+    //返回什么数据？10条有关信息，把req.body的每个字符拆开来，去textArr里面寻找
+    res.send(textData);
+})
+
+function chooseArr(str, arr) {
+    const dataSet = new Set();
+    let charArr = str.split('');
+
+    for (let index = 0; index < charArr.length; index++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j].includes(charArr[index])) {
+                dataSet.add(arr[j]);
+            }
+
+        }
+
+    }
+    const dataArr = Array.from(dataSet)
+
+
+
+    return dataArr;
+}
+
+
+
+
+
+
+
+
 //4、监听窗口启动服务
 app.listen(8000, () => {
     console.log("服务器已经启动,8000端口监听中...");
